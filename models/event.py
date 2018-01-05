debc96d489e54c2782c94a022ba0a1f2935de2b3
Bo Beyer-Johnson <bobeyerjohnson@gmail.com>
2018-01-05 11:59:19 -0500

    updates to handle multiple shard keys and saving of date files

--- models/event.py
+++ models/event.py
@@ -7,11 +7,9 @@ Event class to handle the creation of a event and associated meta data
 
 class Event(object):
 
-    def __init__(self, user_id, event_name, event_dict, ts, session_id):
-        self.user_id = user_id
-        self.shard_key1 = event_dict['shardkeys'][0]
-        self.shard_key2 = event_dict['shardkeys'][1]
-        self.shard_key3 = event_dict['shardkeys'][2]
+    def __init__(self,primary_shard_key_name, primary_shard_key_value, event_name, event_dict, ts, session_id):
+        self.primary_shard_key_name = primary_shard_key_name
+        self.primary_shard_key_value = primary_shard_key_value
         self.event_name = event_name
         self.event_dict = event_dict
         self.ts = ts
@@ -19,16 +17,14 @@ class Event(object):
 
     def generate_event(self):
         event = dict()
-        event[self.shard_key1] = str(self.user_id)
+        event[self.primary_shard_key_name] = str(self.primary_shard_key_value)
         event['event'] = self.event_name
         event['session_id'] = str(self.session_id)
         for property_name, values in self.event_dict.items():
-            # check to make sure we do not treat the event and shardkeys columns as properties
-            if property_name not in ['event', 'shardkeys']:
-                #get the number of property values for each property
-                values_length = len(values)
-                event[property_name] = self.event_dict[property_name][(round(values_length * (random.uniform(0,1)))-1)]
-                event['ts'] = self.ts.strftime("%Y-%m-%d %H:%M:%S")
+            #get the number of property values for each property
+            values_length = len(values)
+            event[property_name] = self.event_dict[property_name][(round(values_length * (random.uniform(0,1)))-1)]
+            event['ts'] = self.ts.strftime("%Y-%m-%d %H:%M:%S")
         return event
 
 
--- src/app.py
+++ src/app.py
@@ -16,70 +16,80 @@ and use flows
 #TODO - need to figure out a way to make people churn, later project would be to get people to come back after churning
 #TODO  - make it so that we handle all the shardkeys when ingested from csv file
 ##global values
-output_data_path ='/Users/bobeyer-johnson/Documents/interana/Solutions/event_generator/data_files/'
-user_list_path = '/Users/bobeyer-johnson/Documents/interana/Solutions/event_generator/user_id_list/'
+output_data_path ='/Users/bobeyer-johnson/Documents/interana/Solutions/event_generator_github/csv_event_generator/data_files/user_flow_files/'
+user_list_path = '/Users/bobeyer-johnson/Documents/interana/Solutions/event_generator_github/csv_event_generator/data_files/user_id_list/'
 user_id_list_file_name = 'user_id_list'
+event_file = "/Users/bobeyer-johnson/Downloads/Dummy Data - SocialMessaging Data Set - Events.csv"
+# get the user flows file
+user_flows_file = '/Users/bobeyer-johnson/Downloads/Dummy Data - SocialMessaging Data Set - Flows.csv'
+last_date_run_file ='/Users/bobeyer-johnson/Documents/interana/Solutions/event_generator_github/csv_event_generator/data_files/last_date_run/last_date_script_was_run'
 todays_date = datetime.datetime.now()
-#TODO will need to write to some file and save
-last_date_run = datetime.datetime.now() - datetime.timedelta(days=90)
-number_of_users = 0
+last_date_run = datetime.datetime.now()
 
+# get the last time the script was run from the data file
+with open(last_date_run_file,'r') as date_file:
+    last_date_run = date_file.read()
+    last_date_run =  datetime.datetime.strptime(last_date_run.strip(), "%Y-%m-%d %H:%M:%S")
 
-## check to see if we have a list of user ids, if not create one and save as a csv file
-my_file = Path("{}{}".format(user_list_path,user_id_list_file_name))
-if my_file.is_file():
-    with open("{}{}".format(user_list_path,user_id_list_file_name),'r') as read_user_id_file:
-        shard_key1_list = read_user_id_file.read()
-        shard_key1_list = shard_key1_list.split(',')
-    # add some new users to the file
-    for new_user in range(20):
-        shard_key1_list.append(uuid.uuid4())
-
-else:
-    shard_key1_list = list()
-    for x in range(0, 500):
-        shard_key1 = uuid.uuid4()
-        shard_key1_list.append(shard_key1)
-    with open("{}{}".format(user_list_path,user_id_list_file_name),'w') as write_user_id_file:
-        for shard in shard_key1_list:
-            write_user_id_file.write("{},".format(str(shard)))
-    # set the "last run date" to be 3 months ago so we can back propogate some new data
-    last_date_run = datetime.datetime.now() - datetime.timedelta(days=90)
-
+# check to see if today's current time is earlier than the time right now. if it is run the script
 if last_date_run < todays_date:
-    ## ingest events and properties from csv
-    event_file = "/Users/bobeyer-johnson/Downloads/Dummy Data - SocialMessaging Data Set - Events.csv"
-    # open the file and turn the CSV file into a usable dictionary for later
+
+    # get the event dict
     event_dict = csv_to_dict(event_file)
-    #Loop through all user Ids
-    for shard_key1 in range(10):
-    #for id in shard_key1_list:
-        # get the user flows file
-        user_flows_file = '/Users/bobeyer-johnson/Downloads/Dummy Data - SocialMessaging Data Set - Flows.csv'
+    # get the shard keys to figure out what actors we have
+    shard_keys = event_dict['shardkeys']
+    #the first shard key in the csv file/list will be used as the primary shard key for the sake of data generation. lets save it for now
+    primary_shark_key = shard_keys[0]
+
+    ## check to see if we have a list of the primary shard key, if not create one and save as a csv file
+    my_file = Path("{}{}".format(user_list_path,user_id_list_file_name))
+    if my_file.is_file():
+        with open("{}{}".format(user_list_path,user_id_list_file_name),'r') as read_user_id_file:
+            primary_shard_key_list = read_user_id_file.read()
+            primary_shard_key_list = primary_shard_key_list.split(',')
+        # add some new users to the file - this is mostly so we can change metrics like retention and do "new user" analysis
+        for new_user in range(20):
+            primary_shard_key_list.append(uuid.uuid4())
+
+    else:
+        primary_shard_key_list = list()
+        for x in range(0, 500):
+            shard_key1 = uuid.uuid4()
+            primary_shard_key_list.append(shard_key1)
+        with open("{}{}".format(user_list_path,user_id_list_file_name),'w') as write_user_id_file:
+            for shard in primary_shard_key_list:
+                write_user_id_file.write("{},".format(str(shard)))
+        # set the "last run date" to be 3 months ago so we can back propogate some new data
+        last_date_run = datetime.datetime.now() - datetime.timedelta(days=90)
+    #get the last date the script was run to see if
+
+        #Loop through all user Ids
+    for shard_key_value in range(10):
+    #for id in primary_shard_key_list:
         # create the user
-        user = User(userId=shard_key1, last_date_run=last_date_run, user_flows_file_location=user_flows_file, event_dict_file_location=event_file)
+        user = User(primary_shard_key_value=shard_key_value, last_date_run=last_date_run, user_flows_file_location=user_flows_file, event_dict_file_location=event_file)
         # format the flows from the csv file into usable JSON
         user_flows = user.flow_dict
         # generate all user events and flows for the users
         data = user.generate_flows(user_flows)
         #make sure we returned some events in our data object
         if len(data) < 1:
-            del data
-        #check the date of the last event and if it isn't to the current run date re-run user.generate_flows to get more events
-        dt = datetime.datetime.strptime(data[len(data)-1]['ts'], "%Y-%m-%d %H:%M:%S")
-        while dt <= todays_date:
-            more_data = user.generate_flows(user_flows)
-            for line in more_data:
-                data.append(line)
-                # make sure to assign the most up to data
-                dt =  datetime.datetime.strptime(line['ts'], "%Y-%m-%d %H:%M:%S")
-        # put in json and write to file
-        with open('{}user_{}'.format(output_data_path,shard_key1),'w') as f:
-            f.write(json.dumps(data))
-
-    ##TODO make it so that 'last_date_run equals today and write to a file
-
-
+            pass
+        else:
+            #check the date of the last event and if it isn't to the current run date re-run user.generate_flows to get more events
+            dt = datetime.datetime.strptime(data[len(data)-1]['ts'], "%Y-%m-%d %H:%M:%S")
+            while dt <= todays_date:
+                more_data = user.generate_flows(user_flows)
+                for line in more_data:
+                    data.append(line)
+                    # make sure to assign the most up to data
+                    dt =  datetime.datetime.strptime(line['ts'], "%Y-%m-%d %H:%M:%S")
+            # put in json and write to file
+            with open('{}user_{}'.format(output_data_path, shard_key_value), 'w') as f:
+                f.write(json.dumps(data))
+    # once all events have been generated make it so the date file is updated to reflect the most recent date and hour the script was run
+    with open(last_date_run_file,'w') as date_file:
+        date_file.write(todays_date.strftime("%Y-%m-%d %H:%M:%S"))
 
 
     ## check to see if lookup table is created, if not create it and write to file as JSON
