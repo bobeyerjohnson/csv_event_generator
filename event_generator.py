6345e06cc3a018761e5d809f1a02b8d103161604
bobeyerjohnson <bobeyerjohnson@gmail.com>
2018-01-08 15:34:05 -0500

    get the folder structure right, damnit

--- event_generator.py
+++ event_generator.py
@@ -69,46 +69,44 @@ def generate_data(all_ids):
     with open("{}{}".format(last_date_run_file_location, last_date_run_file), 'w') as date_file:
         date_file.write(todays_date.strftime("%Y-%m-%d %H:%M:%S"))
 
-
-#start of app
-# get the last time the script was run from the data file
-date_file_check = Path("{}{}".format(last_date_run_file_location,last_date_run_file))
-if date_file_check.is_file():
-    with open("{}{}".format(last_date_run_file_location,last_date_run_file), 'r') as date_file:
-        last_date_run = date_file.read()
-        last_date_run = datetime.datetime.strptime(last_date_run.strip(), "%Y-%m-%d %H:%M:%S")
-else:
-    with open("{}{}".format(last_date_run_file_location,last_date_run_file),'w') as date_file:
-        last_date_run = datetime.datetime.now()-datetime.timedelta(days=90)
-        date_file.write(str(last_date_run))
-
-# check to see if today's current time is earlier than the time right now. if it is run the script
-if last_date_run < todays_date:
-    # get the event dict
-    event_dict = csv_to_dict(event_file)
-    ## check to see if we have a list of the primary shard key, if not create one and save as a csv file
-    my_file = Path("{}{}.json".format(user_list_path, user_id_list_file_name))
-    if my_file.is_file():
-        with open("{}{}.json".format(user_list_path, user_id_list_file_name), 'r') as read_user_id_file:
-            primary_shard_key_dict = json.loads(read_user_id_file.read())
-            primary_shard_key_dict = primary_shard_key_dict
-        # add some new users to the file - this is mostly so we can change metrics like retention and do "new user" analysis
-        primary_shard_key_dict = get_new_users(primary_shard_key_dict=primary_shard_key_dict,
-                                               number_of_users=new_user_to_generate_per_period)
-        generate_data(primary_shard_key_dict)
-
+if __name__ == '__main__':
+    #start of app
+    # get the last time the script was run from the data file
+    date_file_check = Path("{}{}".format(last_date_run_file_location,last_date_run_file))
+    if date_file_check.is_file():
+        with open("{}{}".format(last_date_run_file_location,last_date_run_file), 'r') as date_file:
+            last_date_run = date_file.read()
+            last_date_run = datetime.datetime.strptime(last_date_run.strip(), "%Y-%m-%d %H:%M:%S")
     else:
-        # create a dictionary of users where the key is the user id and store additional details about the user in another dict
-        primary_shard_key_dict = dict()
-        for x in range(1000):
-            shard_key1 = str(uuid.uuid4())
-            user_probability = random.uniform(0, 1)
-            primary_shard_key_dict[shard_key1] = dict()
-            primary_shard_key_dict[shard_key1]['user_probability'] = user_probability
-            primary_shard_key_dict[shard_key1]['churned'] = 'False'
-        # set the "last run date" to be 3 months ago so we can back propogate some new data
-        last_date_run = datetime.datetime.now() - datetime.timedelta(days=90)
-        generate_data(primary_shard_key_dict)
+        with open("{}{}".format(last_date_run_file_location,last_date_run_file),'w') as date_file:
+            last_date_run = datetime.datetime.now()-datetime.timedelta(days=90)
+            date_file.write(str(last_date_run))
 
+    # check to see if today's current time is earlier than the time right now. if it is run the script
+    if last_date_run < todays_date:
+        # get the event dict
+        event_dict = csv_to_dict(event_file)
+        ## check to see if we have a list of the primary shard key, if not create one and save as a csv file
+        my_file = Path("{}{}.json".format(user_list_path, user_id_list_file_name))
+        if my_file.is_file():
+            with open("{}{}.json".format(user_list_path, user_id_list_file_name), 'r') as read_user_id_file:
+                primary_shard_key_dict = json.loads(read_user_id_file.read())
+                primary_shard_key_dict = primary_shard_key_dict
+            # add some new users to the file - this is mostly so we can change metrics like retention and do "new user" analysis
+            primary_shard_key_dict = get_new_users(primary_shard_key_dict=primary_shard_key_dict,
+                                                   number_of_users=new_user_to_generate_per_period)
+            generate_data(primary_shard_key_dict)
 
-#TODO check to see if lookup table is created, if not create it and write to file as JSON
+        else:
+            # create a dictionary of users where the key is the user id and store additional details about the user in another dict
+            primary_shard_key_dict = dict()
+            for x in range(1000):
+                shard_key1 = str(uuid.uuid4())
+                user_probability = random.uniform(0, 1)
+                primary_shard_key_dict[shard_key1] = dict()
+                primary_shard_key_dict[shard_key1]['user_probability'] = user_probability
+                primary_shard_key_dict[shard_key1]['churned'] = 'False'
+            # set the "last run date" to be 3 months ago so we can back propogate some new data
+            last_date_run = datetime.datetime.now() - datetime.timedelta(days=90)
+            generate_data(primary_shard_key_dict)
+    #TODO check to see if lookup table is created, if not create it and write to file as JSON
