48689e1a73afdc035e4c719f65ea3384d73172d6
bobeyerjohnson <bobeyerjohnson@gmail.com>
2018-01-08 20:59:22 -0500

    fix events being generated after todays date

--- event_generator.py
+++ event_generator.py
@@ -23,8 +23,8 @@ event_file = "/Users/bobeyer-johnson/Downloads/Dummy Data - SocialMessaging Data
 user_flows_file = '/Users/bobeyer-johnson/Downloads/Dummy Data - SocialMessaging Data Set - Flows.csv'
 last_date_run_file_location = '/Users/bobeyer-johnson/Documents/interana/Solutions/event_generator_github2/csv_event_generator/src/data_files/last_date_run/'
 last_date_run_file = 'last_date_script_was_run'
-todays_date = datetime.datetime.now()
-last_date_run = datetime.datetime.now()
+today_date = datetime.datetime.now()
+last_date_run = 0
 new_user_to_generate_per_period = 20
 
 def generate_data(all_ids):
@@ -46,14 +46,18 @@ def generate_data(all_ids):
         if len(data) > 1:
             # check the date of the last event and if it isn't to the current run date re-run user.generate_flows to get more events
             dt = datetime.datetime.strptime(data[len(data) - 1]['ts'], "%Y-%m-%d %H:%M:%S")
-            while dt <= todays_date:
+            while dt <= today_date:
                 more_data, churn = user.generate_flows(user_flows)
                 for line in more_data:
                     # make sure to assign the most up to data
                     dt = datetime.datetime.strptime(line['ts'], "%Y-%m-%d %H:%M:%S")
-                    data.append(line)
+                    # break out of the look if the date is past today, otherwise add to the event array
+                    if dt >= today_date:
+                        break
+                    else:
+                        data.append(line)
             # put in json and write to file
-            with gzip.open('{}user_{}_{}.gz'.format(output_data_path, shard_key, todays_date.strftime("%Y-%m-%d")), 'w') as f:
+            with gzip.open('{}user_{}_{}.gz'.format(output_data_path, shard_key, today_date.strftime("%Y-%m-%d")), 'w') as f:
                 f.write(json.dumps(data).encode(errors='ignore'))
     # create a list of all ids that should now churn
     ids_to_remove = [ids for ids in primary_shard_key_dict if primary_shard_key_dict[ids]['churned'] == True]
@@ -67,10 +71,10 @@ def generate_data(all_ids):
     # once all events have been generated make it so the date file is updated
     # to reflect the most recent date and hour the script was run
     with open("{}{}".format(last_date_run_file_location, last_date_run_file), 'w') as date_file:
-        date_file.write(todays_date.strftime("%Y-%m-%d %H:%M:%S"))
+        date_file.write(today_date.strftime("%Y-%m-%d %H:%M:%S"))
 
+#start of app
 if __name__ == '__main__':
-    #start of app
     # get the last time the script was run from the data file
     date_file_check = Path("{}{}".format(last_date_run_file_location,last_date_run_file))
     if date_file_check.is_file():
@@ -80,10 +84,10 @@ if __name__ == '__main__':
     else:
         with open("{}{}".format(last_date_run_file_location,last_date_run_file),'w') as date_file:
             last_date_run = datetime.datetime.now()-datetime.timedelta(days=90)
-            date_file.write(str(last_date_run))
+            date_file.write(last_date_run.strftime("%Y-%m-%d %H:%M:%S"))
 
     # check to see if today's current time is earlier than the time right now. if it is run the script
-    if last_date_run < todays_date:
+    if last_date_run < today_date:
         # get the event dict
         event_dict = csv_to_dict(event_file)
         ## check to see if we have a list of the primary shard key, if not create one and save as a csv file
@@ -100,7 +104,7 @@ if __name__ == '__main__':
         else:
             # create a dictionary of users where the key is the user id and store additional details about the user in another dict
             primary_shard_key_dict = dict()
-            for x in range(1000):
+            for x in range(20):
                 shard_key1 = str(uuid.uuid4())
                 user_probability = random.uniform(0, 1)
                 primary_shard_key_dict[shard_key1] = dict()
